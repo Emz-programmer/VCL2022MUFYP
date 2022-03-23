b8c95b3ddfc2e60e99c2fbe1f685420bdb354de7
Emily <hynesemily99@gmail.com>
2022-03-23 15:14:05 +0000

    updated readme#

---
+++ setup.sh
@@ -0,0 +1,25 @@
+apt-get -qq -y install cmake
+apt-get -qq -y install libpython2.7-dev
+apt-get -qq -y install libblas-dev liblapack-dev libglew-dev
+apt-get -qq -y install build-essential
+apt-get -qq -y install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev  libswscale-dev sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libdc1394-22-dev
+apt-get -qq -y install libeigen3-dev
+cd ~
+git clone https://github.com/opencv/opencv.git
+cd ~/opencv
+mkdir release
+cd release
+cmake -DCMAKE_BUILD_TYPE=RELEASE –DCMAKE_INSTALL_PREFIX=/usr/local ..
+make
+make install
+cd ~
+git clone https://github.com/dorian3d/DBoW2.git
+cd DBoW2
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make
+cd ~
+cd DBoW2
+git clone https://github.com/Emz-programmer/VCL2022MUFYP.git
+cd VCL2022MUFYP
